3132f4727758c4df2151b2ce08a81b5cb7606e96
manish295 <mk2194@scarletmail.rutgers.edu>
2024-04-16 21:01:35 -0400

    Started work on GPS

---
+++ src/components/GPS.svelte
@@ -0,0 +1,24 @@
+
+<script>
+    import * as L from "leaflet";
+
+    import { onMount } from "svelte";
+
+    let map;
+    let mapElem;
+    
+    onMount(() => {
+        map = L.map(mapElem).setView([51.505, -0.09], 13);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+    })
+
+
+</script>
+
+
+
+<div class="map" bind:this={mapElem}>
+</div>

d59e9be5e89116369fa2165ac4217847366d790c
manish295 <mk2194@scarletmail.rutgers.edu>
2024-05-13 12:40:47 -0400

    tinkering with map

--- src/components/GPS.svelte
+++ src/components/GPS.svelte
@@ -1,5 +1,6 @@
 
 <script>
+    import { usa } from "./usa";
     import * as L from "leaflet";
 
     import { onMount } from "svelte";
@@ -17,12 +18,12 @@
     }
     
     onMount(() => {
-        map = L.map(mapElem).setView([40.5, -74.5], 13);
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
+        map = L.map(mapElem).setView([40.5, -74.5], 0.01);
+        L.geoJSON(usa, {
+            // maxZoom: 1,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
-        marker = L.marker([40.5, -74.5]).addTo(map)
+        marker = L.marker([0,0]).addTo(map)
         updatePosition();
     })
 
@@ -31,5 +32,5 @@
 
 
 
-<div class="map" bind:this={mapElem} style="width: 600px; height: 500px;">
+<div class="map" bind:this={mapElem} style="width: 778px; height: 617px;">
 </div>

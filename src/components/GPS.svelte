66af93c5f72fde80ff24c3b47a888ca09276c66e
manish295 <mk2194@scarletmail.rutgers.edu>
2024-05-14 16:29:48 -0400

    made gps offline

--- src/components/GPS.svelte
+++ src/components/GPS.svelte
@@ -1,6 +1,5 @@
 
 <script>
-    import { usa } from "./usa";
     import * as L from "leaflet";
 
     import { onMount } from "svelte";
@@ -13,17 +12,46 @@
     function updatePosition() {
         let lat = data.latitude[data.latitude.length-1];
         let lng = data.longitude[data.longitude.length-1];
-        let latlng = L.latLng(lat, lng);
-        marker.LatLng(latlng);
+        let latlngPnt = map.project([lat, lng], map.getMaxZoom() - 1);
+        marker.LatLng(latlngPnt);
     }
     
     onMount(() => {
-        map = L.map(mapElem).setView([40.5, -74.5], 0.01);
-        L.geoJSON(usa, {
-            // maxZoom: 1,
+        // map = L.map(mapElem);
+        // L.geoJSON(usa, {
+        //     // maxZoom: 1,
+        //     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        // }).addTo(map);
+        // marker = L.marker([0,0]).addTo(map)
+        map = L.map(mapElem, {
+            minZoom: 1,
+            maxZoom: 3,
+            center: [0, 0],
+            zoom: 1,
+            crs: L.CRS.Simple,
+        });
+        var w = 1500;
+        var h = 1000;
+        var url = 'src/engineering.png';
+        var southWest = map.unproject([ 0, h], map.getMaxZoom()-1);
+        var northEast = map.unproject([ w, 0], map.getMaxZoom()-1);
+        var bounds = new L.LatLngBounds( southWest, northEast);
+    //    var bounds = [[0, 0], [1000, 1000]];
+        L.imageOverlay( url, bounds, {
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+
         }).addTo(map);
-        marker = L.marker([0,0]).addTo(map)
+        
+
+        map.setMaxBounds(bounds);
+        let latlngPnt = map.project([40.52, -74.46]);
+        let bnds = map.getBounds();
+        
+        marker = L.marker(map.unproject([500, 500], map.getMaxZoom() - 1)).addTo(map)
+        //148.92, 81.04
+        // map.fitBounds(bounds)
+        // console.log(map.getBounds())
+
         updatePosition();
     })
 
@@ -32,5 +60,5 @@
 
 
 
-<div class="map" bind:this={mapElem} style="width: 778px; height: 617px;">
+<div class="map" bind:this={mapElem} style="width: 590px; height: 450px;">
 </div>
